ee41f657193bd6784c306cbe6b3f215962e0b535
imbobby71 <imbobby71@gmail.com>
2024-12-11 10:14:13 +0900

    20241211

--- src/main/java/com/office/booklink/library/api/ApiController.java
+++ src/main/java/com/office/booklink/library/api/ApiController.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.office.booklink.library.LibraryDto;
 import com.office.booklink.library.LibraryService;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
@@ -43,17 +45,23 @@ public class ApiController {
     		return null;
     	} else if (accCount > 0) {
     		try {
-				
     			apiService.addCount(userIp);
 			} catch (Exception e) {
 	    		response.sendError(404,"장난치지 말아라~~~");
+				return null;
 			}
     	} else {
     		apiService.addNew(userIp);
     	}
     	
+    	try {
     	List<LibraryDto> libraries = libraryService.searchRegion(region);
             return libraries;
+		} catch (Exception e) {
+			response.sendError(500,"데이터베이스 접속 에러");
+			return null;
+		}
+
     }
     
     @GetMapping("/name/{name}")
@@ -65,15 +73,24 @@ public class ApiController {
     		response.sendError(404,"사용 횟수 초과");
     		return null;
     	} else if (accCount > 0) {
-    		apiService.addCount(userIp);
+    		try {
+    			apiService.addCount(userIp);
+			} catch (Exception e) {
+	    		response.sendError(404,"장난치지 말아라~~~");
+			}
     	} else {
     		apiService.addNew(userIp);
     	}
     	
-    	List<LibraryDto> libraries = libraryService.searchName(name);
-    	System.out.println(libraries);
-            return libraries;
+    	try {
+    		List<LibraryDto> libraries = libraryService.searchName(name);
+    		return libraries;
+		} catch (Exception e) {
+			response.sendError(500,"데이터베이스 접속 에러");
+			return null;
+		}
     }
     
+
     
 }
