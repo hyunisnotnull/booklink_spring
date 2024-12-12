a3f0e5ec567d04f5067a93630e99eafbe8fcc8e2
imbobby71 <imbobby71@gmail.com>
2024-12-12 16:21:56 +0900

    20241212

--- src/main/java/com/office/booklink/library/LibraryRepository.java
+++ src/main/java/com/office/booklink/library/LibraryRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LibraryRepository extends JpaRepository<LibraryEntity, Integer>{
+public interface LibraryRepository extends JpaRepository<LibraryEntity, String>{
 
 	
 }
